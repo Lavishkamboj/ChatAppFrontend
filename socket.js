882f0e292bff917016286f52d303c2b5b8aca203
lavishkamboj <lavishkamboj16@gmail.com>
2026-06-14 11:43:42 +0530

    hlo

--- socket.js
+++ socket.js
@@ -17,7 +17,7 @@
 
 import { io } from "socket.io-client";
 
-const res1 = await fetch("https://chatappbackend-prhttps://chat-app-backend-five-murex.vercel.app/oduction-40a2.up.railway.app/me", { credentials: "include" });
+const res1 = await fetch("https://chatappbackend-production-40a2.up.railway.app/me", { credentials: "include" });
 // const res2 = await fetch("http://localhost:8000/conversation", { credentials: "include" });
 const data1 = await res1.json();
 // const data2 = await res2.json();
